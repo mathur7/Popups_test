a7f75713173deddb88c05bb40c8da25de50f86ce
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-06-18 11:29:48 -0700

    integrated theme into test app

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require assets/libraries/isotope/jquery.isotope
+//= require assets/js/gmap3.infobox
+//= require assets/js/gmap3.clusterer
+//= require assets/js/map
+//= require assets/libraries/jquery-bxslider/jquery.bxslider
+//= require assets/libraries/flexslider/jquery.flexslider
+//= require assets/js/jquery.chained.min
+//= require assets/js/realocation
+//= require_tree .
